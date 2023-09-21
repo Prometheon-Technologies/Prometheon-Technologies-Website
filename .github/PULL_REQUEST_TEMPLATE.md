7503f00b2cb82970bcc249003f324f2fabc3f516
DaOfficialWizard <45744329+ZanzyTHEbar@users.noreply.github.com>
2023-09-21 17:45:51 +0100

    refactor: initial commit

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,14 @@
+# Description
+
+**Related issue (if applicable):** fixes #<EyeTrackVR issue number goes here>
+
+## Checklist
+
+<!-- - [ ] The pull request is done against the latest development branch
+- [ ] Only relevant files were touched
+- [ ] Code change compiles without warnings
+- [ ] The code change is tested and works with EyeTrackVR core ESP32 newest release -->
+
+- [ ] I accept the [CLA](https://github.com/RedHawk989/EyeTrackVR/blob/main/repo-tools/CONTRIBUTING.md#contributor-license-agreement-cla).
+
+<!-- _NOTE: The code change must pass CI tests. **Your PR cannot be merged unless tests pass**_ -->
---
+++ app/vite.config.ts
@@ -0,0 +1,46 @@
+import { resolve } from 'path'
+import { optimizeLodashImports } from '@optimize-lodash/rollup-plugin'
+import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
+
+export default defineConfig({
+    clearScreen: false,
+    envPrefix: ['VITE_', 'TAURI_'],
+    resolve: {
+        alias: {
+            '@interfaces': resolve(__dirname, './src/interfaces'),
+            '@components': resolve(__dirname, './src/components'),
+            '@routes': resolve(__dirname, './src/routes'),
+            '@pages': resolve(__dirname, './src/pages'),
+            '@styles': resolve(__dirname, './src/styles'),
+            '@config': resolve(__dirname, './src/config'),
+            '@src': resolve(__dirname, './src'),
+            '@assets': resolve(__dirname, './assets'),
+            '@hooks': resolve(__dirname, './src/utils/hooks'),
+            '@store': resolve(__dirname, './src/store'),
+            '@static': resolve(__dirname, './src/static'),
+            '@utils': resolve(__dirname, './src/utils'),
+        },
+    },
+    plugins: [solidPlugin(), optimizeLodashImports()],
+    server: {
+        port: 3000,
+        host: true,
+        strictPort: true,
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                docs: resolve(__dirname, 'src/windows/docs/index.html'),
+                webserial: resolve(__dirname, 'src/windows/webserial/index.html'),
+            },
+        },
+        // Tauri supports es2021
+        target: ['es2021', 'esnext'],
+        // don't minify for debug builds
+        minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+        // produce sourcemaps for debug builds
+        sourcemap: !!process.env.TAURI_DEBUG,
+    },
+})
