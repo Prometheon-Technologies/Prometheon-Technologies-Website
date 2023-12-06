aae9d3fe66e0b9cb47da0bb843f178510828bf51
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2023-12-06 10:17:48 +0000

    refactor: working on hero

--- app/src/components/CurrentProblem.tsx
+++ app/src/components/CurrentProblem.tsx
@@ -3,11 +3,8 @@ const CurrentProblem = () => {
         <div class="slide fourth" data-slide="fourth">
             <div class="wrapper full_height in_the_middle">
                 <div class="fourth_text">
-                    Existed renewable energy technologies alternative to fossil fuels cannot cover
-                    the global energy needs due to its
-                    <span class="nowrap">
-                        negligible <span class="earth_energy"> advancement</span>
-                    </span>
+                    Bringing global talent together to solve energy inefficiency in the world through
+                    innovation and technology.
                 </div>
             </div>
         </div>
--- app/src/components/Hero.tsx
+++ app/src/components/Hero.tsx
@@ -6,11 +6,7 @@ const Hero = () => {
             <div class="wrapper full_height in_the_middle">
                 <div class="windmill" />
                 <div class="fifth_content">
-                    They work around its theoretical
-                    <span class="nowrap">
-                        maximum <span class="eco" /> and strongly
-                    </span>
-                    depend on ambient conditions.
+                     
                 </div>
             </div>
         </div>
